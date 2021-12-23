bc67cdf9ff54ced939b089b31525de841fcbc3fa
Riccardo Feletig <riccardofeletig@rocketmail.com>
2021-12-23 14:20:32 +0100

    Added black and white works

--- src/components/DrawingList.vue
+++ src/components/DrawingList.vue
@@ -21,11 +21,11 @@
         <div class="max-w-2xl mx-auto py-8 px-4 sm:py-10 sm:px-6 lg:max-w-7xl lg:px-8">
             <div class="grid grid-cols-1 gap-y-24 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                 <div class="group" v-for="image in filteredImages" :key="image.Name">
-                    <div class="h-full bg-gray-200 rounded-lg overflow-hidden border border-gray-800">
+                    <div class="h-full bg-transparent rounded-lg overflow-hidden border border-gray-800">
                         <g-image :src="image.pathLong" alt="William Bondi" class="w-full h-full object-center object-cover group-hover:opacity-75" />
                     </div>
                     <h3 class="mt-1 text-md text-gray-800 font-semibold">
-                        <span class="">{{ image.Name }}</span>
+                       {{ image.Name }}
                     </h3>
                     <span v-if="image.Sold != ''" 
                         class="text-red-600 text-sm font-semibold uppercase">  {{ image.Sold }}</span>
