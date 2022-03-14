e564fe5bf1f91541c775c150b0c82b019026f218
Riccardo Feletig <riccardofeletig@rocketmail.com>
2022-03-14 22:19:20 +0100

    Edited contact us page

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -1,14 +1,21 @@
 <template>
     <div class="bg-white">
         <div class="max-w-7xl mx-auto">
-            <div class="flex my-16 text-center justify-center text-md sm:text-xl font-medium mb-24 md:mb-40 lg:mb-64">
-                <h3 class="text-gray-900">
-                    {{ $t("email")}}: 
-                </h3>
-                <a href="mailto:infowilliambondi@gmail.com"
-                    class="ml-2 text-gray-700 hover:text-gray-900 cursor-pointer underline">
-                    infowilliambondi@gmail.com
-                </a>                    
+            <div class="flex flex-col my-16 mb-24 md:mb-40 lg:mb-64">
+                <div class="flex flex-col text-center justify-center text-md sm:text-xl font-medium">
+                    <a class="mb-4">Steven Vago</a>
+                    <div class="flex text-center justify-center">
+                        <a>Whatsapp: </a>
+                        <a href="tel:+393285853323" class="underline text-gray-700 hover:text-gray-900 mb-2" target="_blank">(+39)328 5853323</a>
+                    </div>
+                    <div class="flex text-center justify-center">
+                        Email: 
+                        <a href="mailto:infowilliambondi@gmail.com"
+                            class="ml-2 text-gray-700 hover:text-gray-900 cursor-pointer underline">
+                            infowilliambondi@gmail.com
+                        </a>             
+                    </div>
+                </div>
             </div>
         </div>
     </div>
